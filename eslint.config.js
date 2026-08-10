71c899484ac909f4beab2e682b904ab2408b66a1
Anna <annasaltveit@gmail.com>
2026-08-10 10:20:39 -0700

    use community fork for dependency conflicts

--- eslint.config.js
+++ eslint.config.js
@@ -2,7 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import jsxA11y from 'eslint-plugin-jsx-a11y'
+import jsxA11y from 'eslint-plugin-jsx-a11y-x'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -39,13 +39,13 @@ export default defineConfig([
       },
     },
     plugins: {
-      'jsx-a11y': jsxA11y,
+      'jsx-a11y-x': jsxA11y,
     },
     rules: {
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
-      ...jsxA11y.flatConfigs.recommended.rules,
-      'jsx-a11y/no-static-element-interactions': 'warn',
-      'jsx-a11y/click-events-have-key-events': 'warn',
+      ...jsxA11y.configs.recommended.rules,
+      'jsx-a11y-x/no-static-element-interactions': 'warn',
+      'jsx-a11y-x/click-events-have-key-events': 'warn',
     },
   },
   {
@@ -57,8 +57,8 @@ export default defineConfig([
   {
     files: ['**/*.{test,spec}.{js,jsx}', 'e2e/**', 'src/test/**'],
     rules: {
-      'jsx-a11y/no-static-element-interactions': 'off',
-      'jsx-a11y/click-events-have-key-events': 'off',
+      'jsx-a11y-x/no-static-element-interactions': 'off',
+      'jsx-a11y-x/click-events-have-key-events': 'off',
     },
   },
 ])
